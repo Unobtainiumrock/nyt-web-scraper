bf585894992b652f492f2aa37ac6c5c1b61423cc
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-22 02:32:01 -0700

    Add a list of what needs to be completed

--- nyt-test.js
+++ nyt-test.js
@@ -3,6 +3,13 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 
+// TO DO:
+// 1) CONVERT THIS TO ASYNC AWAIT SYNTAX TO MATCH OTHER FILES' LOGIC
+// 2) UPDATE THE HANDLEBARS STUFF SO THAT IT IS GETTING PROPS TO RENDER ALL THE 
+  //  IMAGES AND NEW CRAWL DATA.
+// 3) DON'T FORGET TO CONVERT THE NEW CRAWL DATA FROM AN ARRAY-LIKE TO ARRAY
+//    OTHEWISE INSERT MANY WON'T WORK
+
 axios.get('https://www.nytimes.com/section/technology?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Tech&WT.nav=page')
   .then(function(data) {
     const $ = cheerio.load(data.data, {
@@ -34,3 +41,5 @@ axios.get('https://www.nytimes.com/section/technology?action=click&pgtype=Homepa
   }
 
   // recursiveHelper();
+
+  
