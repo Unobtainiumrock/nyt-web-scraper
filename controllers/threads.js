3394a9b95c9d933effef0554729ec58c9e380344
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-22 00:13:30 -0700

    Add threads routes

---
+++ controllers/threads.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+const router = express.Router();
+const scrape = require('../services/scrape/scrape');
+const db = require('../models');
+
+// Route for getting all Articles from the db
+router.get("/threads", (req, res) => {
+  // TODO: Finish the route so it grabs all of the articles
+  db.Article.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/threads/:id", (req, res) => {
+  const id = req.params.id;
+  db.Article.findOne({ _id: id })
+    .populate("note")
+    .then(dbArticle => {
+      res.json(dbArticle);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+});
+
+
+// Route for saving/updating an Article's associated Note
+router.post("/threads/:id", (req, res) => {
+  const id = req.params.id;
+  const note = req.body;
+
+  db.Note.create(note)
+    .then(dbNote => {
+      return db.Article.findOneAndUpdate({ _id: id }, { note: dbNote._id }, { new: true })
+    })
+    .then(dbArticle => {
+      res.json(dbArticle);
+    })
+    .catch(err =>{
+      res.json(err);
+    })
+});
+
+module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,35 @@
 
-// (function test() {
-//   console.log('hello world!');
-//   $.get('/test')
-//     .then(data => {
-//       console.log(data);
-//     });
-// })()
+// Whenever someone clicks a p tag
+$(document).on("click", "h4", function() {
+  // Empty the notes from the note section
+  // $("#notes").empty();
+  // Save the id from the p tag
+  const thisId = $(this).attr("data-id");
 
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/threads/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
 
